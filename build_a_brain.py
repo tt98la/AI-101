36f91c3743730169a8eae25b9463b023f77a9e3b
Tony Troy <tony@tonytroy.com>
2024-10-08 17:41:47 -0700

    - Get the correct answer from the model's predictions (ie. argmax()) - Add labels for the categories and use them for the plot's x-axis and showing the correct answer - Classify 10 random images
    - Add keras lib
    - Remove tensorflow lib (only used for keras)
    - Add numpy

--- build_a_brain.py
+++ build_a_brain.py
@@ -1,54 +1,58 @@
-import tensorflow as tf
+# import tensorflow as tf
 import matplotlib.pyplot as plt
+import keras
+import numpy as np
 
-model = None
-fashion_mnist = tf.keras.datasets.fashion_mnist
+epochs = 5
+model = keras.Sequential
+fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (valid_images, valid_labels) = fashion_mnist.load_data()
 number_of_classes = train_labels.max() + 1  # aka number of neurons / categories
+labels = ["T-shirt/top", "Trouser", "Pullover3", "Dress4", "Coat5", "Sandal6", "Shirt7", "Sneaker8", "Bag9", "Ankle boot"]
 
 def main():
     create_model()
     verify_model()
     train_model()
-    evaluate_model()
-    classify_image()
+    
+    for i in range(10):
+        classify_image(np.random.randint(0, len(train_images)))
 
 def create_model():
     #create the layers
     global model
-    model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(number_of_classes)
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(number_of_classes)
     ])
 
 def verify_model():
     model.summary()
-    #tf.keras.utils.plot_model(model, show_shapes=True)
 
 def train_model():
-    model.compile(optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
+    model.compile(
+        optimizer='adam',
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+        metrics=['accuracy'])
 
-def evaluate_model(epochs=5):
     history = model.fit(
         train_images,
         train_labels,
         epochs=epochs,
         verbose=True,
-        validation_data=(valid_images, valid_labels)
-    )
+        validation_data=(valid_images, valid_labels))
 
 def classify_image(data_idx=42):
     plot_image(data_idx)
+    pred = model.predict(train_images[data_idx:data_idx+1])
 
     x_values = range(number_of_classes)
     plt.figure()
-    plt.bar(x_values, model.predict(train_images[data_idx:data_idx+1]).flatten())
-    plt.xticks(range(number_of_classes))
+    plt.bar(x_values, pred.flatten())
+    plt.xticks(range(number_of_classes), labels=labels, rotation=20)
     plt.show()
     
-    print("correct answer:", train_labels[data_idx])
+    print("correct answer:", labels[pred.argmax()])
 
 def plot_image(data_idx):
     plt.figure()
